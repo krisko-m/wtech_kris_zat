ab29b12e2f35410f4b9d775f4dd2b410a2474e62
krisko-m <m.krisko19@gmail.com>
2026-05-10 14:13:06 +0200

    feat: added working new/hot products row in index

--- kockac/resources/views/index.blade.php
+++ kockac/resources/views/index.blade.php
@@ -16,74 +16,51 @@
     <div class="container-fluid px-4 mt-4">
         <div class="d-flex align-items-baseline justify-content-between mb-3">
             <div class="section-title">NEW / HOT Products</div>
-            <a href="{{ url('/products') }}" class="see-all">Show all →</a>
+            @if(Auth::check() && Auth::user()->is_admin)
+                <a href="{{ url('/admin/products?sort=newest') }}" class="see-all">Show all →</a>
+            @else
+                <a href="{{ url('/products?sort=newest') }}" class="see-all">Show all →</a>
+            @endif
         </div>
 
         <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-xl-5 g-3 pb-5">
-            <div class="col">
-                <a href="{{ url('/products/1') }}" class="product-card">
-                    <div class="product-img">
-                        <img src="{{ asset('assets/products/Bang/product-bang-1.png') }}" alt="Bang">
-                        <div class="product-badge">NEW</div>
-                    </div>
-                    <div class="p-3">
-                        <div class="product-name mb-1">Bang!</div>
-                        <div class="product-price">15,99 €</div>
-                    </div>
-                </a>
-            </div>
 
-            <div class="col">
-                <a href="#" class="product-card">
-                    <div class="product-img">
-                        <img src="{{ asset('assets/product-catan.png') }}" alt="Catan">
-                        <div class="product-badge">HOT</div>
-                    </div>
-                    <div class="p-3">
-                        <div class="product-name mb-1">Catan - Base Game</div>
-                        <div class="product-price">44,99 €</div>
-                    </div>
-                </a>
-            </div>
+            {{-- NEW produkty --}}
+            @foreach($newProducts as $product)
+                <div class="col">
+                    <a href="{{ url('/products/' . $product->product_id) }}" class="product-card">
+                        <div class="product-img">
+                            @if($product->mainImage)
+                                <img src="{{ $product->mainImage->image_path }}" alt="{{ $product->name }}">
+                            @endif
+                            <div class="product-badge">NEW</div>
+                        </div>
+                        <div class="p-3">
+                            <div class="product-name mb-1">{{ $product->name }}</div>
+                            <div class="product-price">{{ number_format($product->price, 2) }} €</div>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
 
-            <div class="col">
-                <a href="#" class="product-card">
-                    <div class="product-img">
-                        <img src="{{ asset('assets/product-hitster.png') }}" alt="Hitster">
-                        <div class="product-badge">HOT</div>
-                    </div>
-                    <div class="p-3">
-                        <div class="product-name mb-1">Hitster - The Music Party Game</div>
-                        <div class="product-price">24,99 €</div>
-                    </div>
-                </a>
-            </div>
+            {{-- HOT produkty --}}
+            @foreach($hotProducts as $product)
+                <div class="col">
+                    <a href="{{ url('/products/' . $product->product_id) }}" class="product-card">
+                        <div class="product-img">
+                            @if($product->mainImage)
+                                <img src="{{ $product->mainImage->image_path }}" alt="{{ $product->name }}">
+                            @endif
+                            <div class="product-badge hot">HOT</div>
+                        </div>
+                        <div class="p-3">
+                            <div class="product-name mb-1">{{ $product->name }}</div>
+                            <div class="product-price">{{ number_format($product->price, 2) }} €</div>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
 
-            <div class="col">
-                <a href="#" class="product-card">
-                    <div class="product-img">
-                        <img src="{{ asset('assets/product-uno.png') }}" alt="Uno">
-                        <div class="product-badge">NEW</div>
-                    </div>
-                    <div class="p-3">
-                        <div class="product-name mb-1">UNO</div>
-                        <div class="product-price">9,99 €</div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col">
-                <a href="#" class="product-card">
-                    <div class="product-img">
-                        <img src="{{ asset('assets/product-witcherow.png') }}" alt="WitcherOW">
-                        <div class="product-badge">NEW</div>
-                    </div>
-                    <div class="p-3">
-                        <div class="product-name mb-1">The Witcher - Old World</div>
-                        <div class="product-price">79,99 €</div>
-                    </div>
-                </a>
-            </div>
         </div>
     </div>
 
